30ddff33fbfd0681c0aec9674f6c8c5a0530e405
Detrick Lewis <99450825+Detricklew@users.noreply.github.com>
2024-12-01 07:54:14 -0600

    Time: 71 ms (11.84%), Space: 74.7 MB (22.98%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        std::set<int> check;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if (check.contains(nums[i])){
+                return true;
+            }
+            check.insert(nums[i]);
+        }
+        return false;
+    }
+};
