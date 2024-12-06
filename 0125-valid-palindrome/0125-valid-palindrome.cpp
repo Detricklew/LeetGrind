986f875667be2ec958b07a632fbcadbd6b1ac17c
Detrick Lewis <99450825+Detricklew@users.noreply.github.com>
2024-12-05 20:49:54 -0600

    Time: 43 ms (7.06%), Space: 10.2 MB (5.13%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,36 @@
+#include <cctype>
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        for (int i = 0; i < s.size(); i++ ){
+            s[i] = std::tolower(s[i]);
+        }
+
+        int l,r;
+        l = 0;
+        r = s.size() - 1;
+
+        while (l < r) {
+            if (!(s[l] >= 48 && s[l] < 58) && !(s[l] >= 65 && s[l] < 91) && !(s[l] >= 97 && s[l] < 123) ){
+                 printf("skipped %c %d\n", s[l], int(s[l]));
+                l++;
+                continue;
+            }
+
+            if (!(s[r] >= 48 && s[r] < 58) && !(s[r] >= 65 && s[r] < 91) && !(s[r] >= 97 && s[r] < 123) ){
+                printf("skipped %c\n", s[r]);
+                r--;
+                continue;
+            }
+
+            
+            if (s[l++] != s[r--]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
