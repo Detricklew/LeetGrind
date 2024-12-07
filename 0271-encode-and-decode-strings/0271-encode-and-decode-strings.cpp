ebe3a370a752cb25cfd95a97f19f4d5aa583626d
Detrick Lewis <99450825+Detricklew@users.noreply.github.com>
2024-12-07 06:56:41 -0600

    Time: 39 ms (22%), Space: 24.9 MB (78.46%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.cpp
@@ -0,0 +1,40 @@
+class Codec {
+public:
+
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        string res = "";
+
+        for (string str : strs){
+            for (char c : str){
+                res += c - 'a';
+            }
+            res += "{}";
+            
+        }
+        cout<< res.length() << endl;
+        return res;
+    }
+
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
+        vector<string> res;
+        string cur = "";
+        for (int i = 0; i < s.size(); i++){
+            if((i < s.size() - 1) && s[i] == '{' &&  s[i+1] == '}'){
+                res.push_back(cur);
+                cur = "";
+                i++;
+                continue;
+            }
+            int holda = s[i] + 'a';
+            cur += static_cast<char>(holda);
+        }
+
+        return res;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.decode(codec.encode(strs));
