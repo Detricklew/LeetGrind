ab3a174fdfb8672cf422c6119ccbbadd348bf7cb
Detrick Lewis <99450825+Detricklew@users.noreply.github.com>
2024-12-11 04:12:38 -0600

    Time: 62 ms (14.47%), Space: 31.1 MB (13.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> return_val;
-        std::unordered_map<string, vector<string>> items;
-        
+        unordered_map<string,vector<string>> count_map;
+
         for (string str : strs){
-            int item[26] = {0};
-
-            for (char c : str){
-                item[c - 'a']++;
-            }
-
-
-            string key = std::to_string(item[0]);
+            int count[26] = {0};
             
-            for (int i = 1; i < 26; ++i){
-                key += "," + std::to_string(item[i]);
+            for (char c : str){
+                count[c - 'a']++;
+            }
+            
+            string key = to_string(count[0]) + ',';
+            
+            for (int i = 1; i < 26; i++) {
+                key += to_string(count[i]) + ','; 
             }
 
-            items[key].push_back(str);
+            
+            count_map[key].push_back(str);
+
+        }
+        vector<vector<string>> res;
+        
+        for (const auto & [key, value]: count_map){
+            res.push_back(value);
         }
 
-        for (const auto & [key, value] : items){
-            return_val.push_back(value);
-        }
-
-        return return_val;
+        return res;
     }
 };
