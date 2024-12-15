6cf085fe2d1eb3a32a09838ca9a1b2162f232b48
Detrick Lewis <99450825+Detricklew@users.noreply.github.com>
2024-12-15 08:59:42 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        unordered_map<int,int> mappy_map;
+        vector<int> stacky_stack;
+
+        for(int i = 0; i < position.size(); i++){
+            mappy_map[position[i]] = i;
+        }
+
+        sort(position.begin(), position.end());
+
+        for(int i = position.size()-1; i >= 0; i--){
+            int val = position[i];
+            int time_to = std::ceil(((float)target - (float)val)/(float)speed[mappy_map[val]]);
+            cout<<time_to<<endl;
+            if(!(stacky_stack.size())){
+                stacky_stack.push_back(time_to);
+                continue;
+            }
+
+            if(time_to > stacky_stack.back()){
+                stacky_stack.push_back(time_to);
+            }
+        }
+
+        return stacky_stack.size();
+    }
+};
