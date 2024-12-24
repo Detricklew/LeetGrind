370ed3b00b4d80279f3777b6e1569c8aae73b67a
Detrick Lewis <99450825+Detricklew@users.noreply.github.com>
2024-12-24 06:10:32 -0600

    Time: 0 ms (100%), Space: 9.9 MB (45.66%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -6,6 +6,7 @@ public:
         int hash[26] = {0};
         int hash2[26] = {0};
         int count = 0;
+        
         for (char c : s1){
 
             if(hash[c - 'a']++ == 0){
@@ -22,27 +23,20 @@ public:
             if (match == count) return true;
 
         }
-        cout<<count<<endl;
-        if (hash[s2[l] - 'a'] != 0 && hash2[s2[l] - 'a'] == hash[s2[l] - 'a']) match--;
+        if (hash[s2[l] - 'a'] != 0 && hash2[s2[l] - 'a']-- == hash[s2[l] - 'a']) match--;
         l++;
         while (h < s2.length()){
             if (hash[s2[h] - 'a'] != 0 && hash2[s2[h] - 'a'] == hash[s2[h] - 'a'] - 1){
                  match++;
-                 cout<<"match"<<endl;
-                 cout<<s2[h]<<endl;
             }
             hash2[s2[h] - 'a']++;
             if (match == count) return true;
             
             if (hash[s2[l] - 'a'] != 0 && hash2[s2[l] - 'a'] == hash[s2[l] - 'a']){
                 match--;
-                cout<<"no match"<<endl;
-                cout<<s2[l]<<endl;
             }
+
             hash2[s2[l] - 'a']--;
-            cout<<s2[l]<<endl;
-            cout<<s2[h]<<endl;
-            cout<<match<<endl;
             l++;
             h++;
         }
