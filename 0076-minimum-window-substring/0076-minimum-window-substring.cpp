c6b2679a07dbe8fb681e4e54f921f05568e87a66
Detrick Lewis <99450825+Detricklew@users.noreply.github.com>
2024-12-24 20:34:07 -0600

    Time: 18 ms (43.35%), Space: 11.6 MB (23.46%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -26,15 +26,11 @@ public:
             if(hash2[s[h]]++ == hash[s[h]] - 1) match++;
 
             if (count == match){
-                cout<<"match"<<endl;
-                cout<<s[l]<<endl;
-                cout<<s[h]<<endl;
                 while(count == match){
                     if(hash.find(s[l]) == hash.end()){
                         l++;
                         continue;
                     }
-                    cout<<"yo"<<endl;
                     if(hash2[s[l]]-- == hash[s[l]]){
                         match--;
                         break;
@@ -42,8 +38,6 @@ public:
 
                     l++;
                 }
-                cout<<"smol"<<endl;
-                cout<<l<<endl;
                 if((h-l)+1 < smol.first){
                     smol = {(h-l)+1, {l, h}};
                 }
