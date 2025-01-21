68e06abfe58414d173fd2f0b95c613fb0732e687
Detrick Lewis <99450825+Detricklew@users.noreply.github.com>
2025-01-21 05:15:31 -0600

    Time: 0 ms (100%), Space: 12.9 MB (43.17%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -13,8 +13,8 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if (!p && !q) return true;
-        if (!p && q) return false;
-        if (p && !q) return false;
+        if (!p || !q) return false;
+        //if (p && !q) return false;
 
         if (p->val != q->val) return false;
 
